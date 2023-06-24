3ac4c82c69587cc25ffdd5a6ab7d1fd9a6e3be9b
Giuliano Rasper <rasper.giuliano@gmail.com>
2023-06-24 12:32:22 +0200

    Added some example code for properly incorperating the frontend

--- backend/code/main.py
+++ backend/code/main.py
@@ -1,8 +1,17 @@
 import preprocessing
+from backend.code.location_recommender import LocationRecommender
+
 
 def run():
     companies = preprocessing.get_companies()
-    recommender = LocationRecommender(companies)
+
+    # use a small sample size for testing
+    sample_size = 10
+    recommender = LocationRecommender(companies, sample_size=sample_size)
+    the_target = "dummy"
+    recommender.set_target_tags([the_target])
+    # format as specified in the google docs doc
+    recommendations = recommender.get_attributed_location_recommendations(max_companies=10)
     # TODO: setup HTTP GET endpoint here Endpoints we need:
     #  1. GET /sectors
     #  returns all sectors, i.e. returns a list of strings
